ec3375af551afcafea85a986e1396d2f288cf05e
Jacob Young <contact@jacobyoung.me>
2015-02-21 03:41:38 -0800

    Initialized Main.Storyboard and Modal TableViewController

---
+++ poopiOS/poopiOS/SettingViewController.m
@@ -0,0 +1,13 @@
+//
+//  SettingViewController.m
+//  poopiOS
+//
+//  Created by Jacob Young on 2/21/15.
+//  Copyright (c) 2015 Table21. All rights reserved.
+//
+
+#import "SettingViewController.h"
+
+@implementation SettingViewController
+
+@end
